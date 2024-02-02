1c72e53bf6cb4f5ca84af855b4a5ba603b1d4e2a
sha-bash <shabashev.va@yandex.ru>
2024-02-03 00:52:31 +0400

    task 1 and 2

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -6,15 +6,34 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
+    
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+       
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
+               
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        self.grades = {}  
+    
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if grade > 10:
+                return 'Ошибка: оценка не может быть больше 10'
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
@@ -25,7 +44,7 @@ class Mentor:
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
  
 cool_mentor.rate_hw(best_student, 'Python', 10)
